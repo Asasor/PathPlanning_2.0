cdf82b77a5dcd2186e2aeb43f091e077bb907a7a
Asasor <asaf.soreq@gmail.com>
2020-07-14 11:29:48 +0300

    added basic implementation of ruler

--- pathPlanning_JavaScript.js
+++ pathPlanning_JavaScript.js
@@ -6,18 +6,36 @@ context = canvas.getContext('2d');
 var imageLoader = document.getElementById('backgroundImage');
 imageLoader.addEventListener('change', makeBase, false);
 
+// mode indicators
 var movePointCond = false;
 var reverseCond = false;
+var rulerCoords = [false]; // initiate array to store the coordinates and state of the ruler
+var mirrorModeState = 0; // 0 -> none; 1 -> horizontal; 2 -> vertical
+
 var pointInfo = [];
 
 
 //---------------------- canvas interaction ----------------------
 canvas.addEventListener('click', function(evt)  // left click listener
     {
-        var mousePos = getMousePos(canvas, evt);
+        var mousePos = getMousePos(canvas, evt);  // get the coordinates of the mouse
 
-        pointInfo.push(new Array());
-        pointInfo[pointInfo.length - 1] = [mousePos.x, mousePos.y, reverseCond];
+        if (rulerCoords[0] == false)  // check whether a regular point or a ruler should be drawn
+        {  // add a regular point
+          pointInfo.push(new Array());
+          pointInfo[pointInfo.length - 1] = [mousePos.x, mousePos.y, reverseCond];
+        }
+        
+        else if (rulerCoords.length < 3)  // check if the ruler is active and there are a at most two points
+          { rulerCoords.push([mousePos.x, mousePos.y]); }  // make a ruler
+          
+        else 
+        {
+          clearCanvas();
+          drawPoints(context);
+          drawRuler(context, rulerCoords[1][0], rulerCoords[1][1], rulerCoords[2][0], rulerCoords[2][1]);
+          rulerCoords = [true];
+        }
 
         drawPoints(context);
         // alert("working");
@@ -57,24 +75,40 @@ function deletePoint() {
 function movePoint(Btn) { // not finished; need to implement moving point
   movePointCond ^= 1; // flip bool
   if (movePointCond)
-    { Btn.style.backgroundColor="lightgreen"; }
+    { Btn.style.backgroundColor='gray'; }
   else
-    { Btn.style.backgroundColor='#FA8072'; }
+    { Btn.style.backgroundColor=''; } // reset to default colour
 }
 
+function rulerMode(Btn) {
+  if (rulerCoords[0] == false)
+  {
+    Btn.style.backgroundColor = 'lightgray';
+    rulerCoords[0] = true;
+  }
 
-function Ruler() {
-
+  else
+  {
+    Btn.style.backgroundColor = '';
+    rulerCoords[0] = false;
+  }
 }
 
+function mirrorMode(Btn) {
+  mirrorModeState = (mirrorModeState + 1) % 3; // advance mirror mode to next state
+  if (mirrorModeState == 1)
+    { Btn.style.backgroundColor='lightgray'; } // horizontal mirror mode
+  else if (mirrorModeState == 2)
+    { Btn.style.backgroundColor='gray'; } // vertical mirror mode
+  else
+    {Btn.style.backgroundColor='';} // reset to default colour
 
-function mirrorMode() {
-
+  drawMirror(context, 750, 400); // context, width, height
 }
 
 
 function mirrorPath() {
-  
+
 }
 
 
@@ -91,9 +125,9 @@ function setSegment() {
 function reverseMode(Btn) {
   reverseCond ^= 1; // flip bool
   if (reverseCond)
-    { Btn.style.backgroundColor="lightgreen"; }
+    { Btn.style.backgroundColor="gray"; }
   else
-    { Btn.style.backgroundColor='#FA8072'; }
+    { Btn.style.backgroundColor=''; } // reset to default colour
 }
 
 
@@ -114,11 +148,14 @@ function getMousePos(canvas, evt) {
     };
 }
 
+function refresh(context) { // a bit like clear canvas but retains information that clear canvas erases
+    context.fillStyle = "white"; // change later to background image from makebase instead of white
+    context.fillRect(0, 0, canvas.width, canvas.height);
+}
 
 function drawPoints(context) {
     var start = true;
     var lastPoint = pointInfo[0]; // save the last point at any given time for the arrow's orientation
-
     context.beginPath();
     pointInfo.forEach((element) => {
         if (!start)
@@ -134,6 +171,25 @@ function drawPoints(context) {
     context.stroke();
 }
 
+function drawMirror(context, width, height) {
+    refresh(context);
+    drawPoints(context);
+
+    context.beginPath();
+    if (mirrorModeState == 1)
+      {
+        context.moveTo(0,height/2);
+        context.lineTo(width,height/2);
+      }
+    else if (mirrorModeState == 2)
+      {
+        context.moveTo(width/2,0);
+        context.lineTo(width/2,height);
+      }
+    else
+      { drawPoints(context); }
+    context.stroke();
+}
 
 //  taken from https://stackoverflow.com/questions/808826/draw-arrow-on-canvas-tag
 function arrowTo(context, fromx, fromy, tox, toy) {
@@ -158,4 +214,27 @@ function arrowToReverse(context, fromx, fromy, tox, toy) {
   context.lineTo(tox - headlen * Math.cos(angle - Math.PI / 6), toy - headlen * Math.sin(angle - Math.PI / 6));
   context.moveTo(tox, toy);
   context.lineTo(tox - headlen * Math.cos(angle + Math.PI / 6), toy - headlen * Math.sin(angle + Math.PI / 6));
+}
+
+function drawRuler(context, fromx, fromy, tox, toy) {
+  context.beginPath();
+
+  var headlen = 15; // length of head in pixels
+  var dx = fromx - tox;
+  var dy = fromy - toy;
+  var angle = Math.atan2(dy, dx);
+  context.moveTo(fromx, fromy);
+  context.lineTo(fromx - headlen * Math.cos(angle - Math.PI / 2), fromy - headlen * Math.sin(angle - Math.PI / 2));
+  context.moveTo(fromx, fromy);
+  context.lineTo(fromx - headlen * Math.cos(angle + Math.PI / 2), fromy - headlen * Math.sin(angle + Math.PI / 2));
+  context.moveTo(fromx, fromy);
+  context.lineTo(tox, toy);
+  context.lineTo(tox - headlen * Math.cos(angle - Math.PI / 2), toy - headlen * Math.sin(angle - Math.PI / 2));
+  context.moveTo(tox, toy);
+  context.lineTo(tox - headlen * Math.cos(angle + Math.PI / 2), toy - headlen * Math.sin(angle + Math.PI / 2));
+  context.lineTo(fromx - headlen * Math.cos(angle + Math.PI / 2), fromy - headlen * Math.sin(angle + Math.PI / 2));
+  context.moveTo(tox - headlen * Math.cos(angle - Math.PI / 2), toy - headlen * Math.sin(angle - Math.PI / 2));
+  context.lineTo(fromx - headlen * Math.cos(angle - Math.PI / 2), fromy - headlen * Math.sin(angle - Math.PI / 2));
+
+  context.stroke();
 }
