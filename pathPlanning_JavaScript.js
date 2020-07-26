a7745dcb5e1cb0ddaed020c6fc57677a664a3bb7
Asasor <asaf.soreq@gmail.com>
2020-07-26 12:56:18 +0300

    tried and failed to fix path not showing, will roll back to earlier versions.

--- pathPlanning_JavaScript.js
+++ pathPlanning_JavaScript.js
@@ -1,6 +1,6 @@
-//  use alert() and console.log() to debug
+// use alert() and console.log() to debug
 var canvas = document.getElementById('mainCanvas'),
-context = canvas.getContext('2d');
+    context = canvas.getContext('2d');
 
 var imageLoader = document.getElementById('backgroundImage');
 imageLoader.addEventListener('change', makeBase, false);
@@ -8,53 +8,53 @@ imageLoader.addEventListener('change', makeBase, false);
 // mode indicators
 var movePointCond = false;
 var reverseCond = false;
-var rulerCoords = [false]; // initiate array to store the coordinates and state of the ruler
-var mirrorModeState = 0; // 0 -> none; 1 -> horizontal; 2 -> vertical
+var rulerCoords = [false];  // initiate array to store the coordinates and state of the ruler
+var mirrorModeState = 0;  // 0 -> none; 1 -> horizontal; 2 -> vertical
+var realWidth = 7.5;  // the real width (in meters) of the field which is represented by the canvas
+var realHeight = 4.0;  // the real height (in meters) of the field which is represented by the canvas
+var img;
 
 var pointInfo = [];
 
 
 //---------------------- canvas interaction ----------------------
-canvas.addEventListener('click', function(evt)  // left click listener
-    {
-        var mousePos = getMousePos(canvas, evt);  // get the coordinates of the mouse
+canvas.addEventListener('click', function (evt) { // left click listener
+      
+    var mousePos = getMousePos(canvas, evt);  // get the coordinates of the mouse
 
-        if (rulerCoords[0] == false)  // check whether a regular point or a ruler should be drawn
-        {  // add a regular point
-          pointInfo.push(new Array());
-          pointInfo[pointInfo.length - 1] = [mousePos.x, mousePos.y, reverseCond];
-        }
-        
-        else if (rulerCoords.length < 3)  // check if the ruler is active and there are a at most two points
-          {  
-            rulerCoords.push([mousePos.x, mousePos.y]);
-            refresh(context);
-            drawPoints(context);
-            drawRuler(context, rulerCoords[1][0], rulerCoords[1][1], rulerCoords[2][0], rulerCoords[2][1]);
-            rulerCoords = [true];
-          }  
-
+    if (rulerCoords[0] === false) { // check whether a regular point or a ruler should be drawn
+        // add a regular point
+        pointInfo.push([]);
+        pointInfo[pointInfo.length - 1] = [mousePos.x, mousePos.y, reverseCond];
+    } else if (rulerCoords.length < 3) { // check if the ruler is active and there are a at most two points  
+        rulerCoords.push([mousePos.x, mousePos.y]);
+        refresh(context);
         drawPoints(context);
-        // alert("working");
-    }, false
+        drawRuler(context, rulerCoords[1][0], rulerCoords[1][1], rulerCoords[2][0], rulerCoords[2][1]);
+        rulerCoords = [true]; 
+    }
+
+    drawPoints(context);
+    alert("working");
+}, false
 );
 
 //---------------------- buttons ----------------------
 
 // taken from http://jsfiddle.net/influenztial/qy7h5/
-function makeBase(e){
-  var reader = new FileReader();
-  reader.onload = function(event){
-      var img = new Image();
-      img.onload = function(){
-          canvas.width = img.width;
-          canvas.height = img.height;
-          context.drawImage(img,0,0);
+function makeBase(e){ 
+    var reader = new FileReader();
+    reader.onload = function(event){
+        img = new Image();
+        img.onload = function() {
+            canvas.width = img.width;
+            canvas.height = img.height;
+            context.drawImage(img, 0, 0);
       }
       img.src = event.target.result;
-  }
-  reader.readAsDataURL(e.target.files[0]);
-  clearCanvas();
+      }
+    reader.readAsDataURL(e.target.files[0]);
+    clearCanvas();
 }
 
 
@@ -71,45 +71,35 @@ function deletePoint() {
 
 function movePoint(Btn) { // not finished; need to implement moving point
   movePointCond ^= 1; // flip bool
-  if (movePointCond)
-    { Btn.style.backgroundColor='gray'; }
-  else
-    { Btn.style.backgroundColor=''; } // reset to default colour
+  if (movePointCond) { Btn.style.backgroundColor = 'gray'; } else { Btn.style.backgroundColor = ''; } // reset to default colour
 }
 
 function rulerMode(Btn) {
-  if (rulerCoords[0] == false)
-  {
-    Btn.style.backgroundColor = 'lightgray';
-    rulerCoords[0] = true;
-  }
-
-  else
-  {
-    Btn.style.backgroundColor = '';
-    rulerCoords[0] = false;
-  }
+  if (rulerCoords[0] === false) {
+        Btn.style.backgroundColor = 'lightgray';
+        rulerCoords[0] = true;
+    } else {
+        Btn.style.backgroundColor = '';
+        rulerCoords[0] = false;
+    }
 }
 
 function mirrorMode(Btn) {
-  mirrorModeState = (mirrorModeState + 1) % 3; // advance mirror mode to next state
-  if (mirrorModeState == 1)
-    { Btn.style.backgroundColor='lightgray'; } // horizontal mirror mode
-  else if (mirrorModeState == 2)
-    { Btn.style.backgroundColor='gray'; } // vertical mirror mode
-  else
-    {Btn.style.backgroundColor='';} // reset to default colour
+    mirrorModeState = (mirrorModeState + 1) % 3; // advance mirror mode to next state
+    if (mirrorModeState === 1) { Btn.style.backgroundColor = 'lightgray'; } // horizontal mirror mode
+  else if (mirrorModeState === 2)
+    { Btn.style.backgroundColor = 'gray'; } // vertical mirror mode
+        else {Btn.style.backgroundColor = ''; } // reset to default colour
 
   drawPoints(context);
 }
 
 
 function mirrorPath() {
-  var mirrorPointInfo = JSON.parse(JSON.stringify(pointInfo));  // clone the point locations into a new reversed                                                                       array
+  var mirrorPointInfo = JSON.parse(JSON.stringify(pointInfo));  // clone the point locations into a new reversed array
   mirrorPointInfo.reverse();
 
-  if (mirrorModeState == 1)
-  {
+  if (mirrorModeState === 1) {
   for (i = 0; i < mirrorPointInfo.length; i++)  // mirror points vertically
     {mirrorPointInfo[i][1] += 2 * (parseInt(canvas.height / 2) - mirrorPointInfo[i][1]);}
   } 
@@ -150,9 +140,19 @@ function reverseMode(Btn) {
 
 function clearCanvas() {
   pointInfo = [];
-
-  context.fillStyle = "white"; // change later to background image from makebase instead of white
-  context.fillRect(0, 0, canvas.width, canvas.height);
+  alert(typeof(img.src)); // debugging; remove later.
+  if (typeof(img.src) == string) // if the image is active
+  {
+    canvas.width = img.width;
+    canvas.height = img.height;
+     context.drawImage(img,0,0);
+  }
+  
+  else // otherwise (if the image is not active) 
+  {
+    context.fillStyle = "white"; // change later to background image from makebase instead of white
+    context.fillRect(0, 0, canvas.width, canvas.height);
+  }
   drawPoints(context);
 }
 
@@ -181,16 +181,29 @@ function getMousePos(canvas, evt) {
 }
 
 function refresh(context) { // a bit like clear canvas but retains information that clear canvas erases
+  alert(typeof(img.src)); // debugging; remove later.
+  if (typeof(img.src) == string) // if the image is active
+  {
+    canvas.width = img.width;
+    canvas.height = img.height;
+     context.drawImage(img,0,0);
+  }
+      
+  else // otherwise (if the image is not active) 
+  {
     context.fillStyle = "white"; // change later to background image from makebase instead of white
     context.fillRect(0, 0, canvas.width, canvas.height);
+  }
 }
 
 function drawPoints(context) {
+    //alert("working");
     refresh(context);
     var start = true;
     var lastPoint = pointInfo[0]; // save the last point at any given time for the arrow's orientation
     context.beginPath();
-    pointInfo.forEach((element) => {
+    
+ /*   pointInfo.forEach((element) => {
         if (!start)
         {
         if (!element[2]) // check if reverse mode is toggled
@@ -200,7 +213,8 @@ function drawPoints(context) {
         lastPoint = element;
         }
         start = false;
-    })
+    })  not working fix later */ 
+    
     context.stroke();
 
     drawMirror(context, canvas.width, canvas.height);  // draw mirror
